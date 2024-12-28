be7942f4e48f4625ec3e80ae26d1eb3493014180
tiendeptraibodoithe <tienhaku0412@gmail.com>
2024-12-28 22:01:05 +0700

    first commit

---
+++ Assets/Scripts/Ammo.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammo : MonoBehaviour
+{
+    [SerializeField] private AmmoSlot[] ammoSlots;
+
+    [System.Serializable]
+    private class AmmoSlot
+    {
+        public AmmoType type;
+        public int ammoAmount;      // Đạn trong băng hiện tại
+        public int maxAmmo = 30;    // Số đạn tối đa trong một băng đạn
+        public int reserveAmmo = 90; // Đạn dự trữ
+    }
+
+    public int GetCurrentAmmo(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        return slot != null ? slot.ammoAmount : 0;
+    }
+
+    public int GetMaxAmmo(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        return slot != null ? slot.maxAmmo : 0;
+    }
+
+    public int GetReserveAmmo(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        return slot != null ? slot.reserveAmmo : 0;
+    }
+
+    public void ReduceCurrentAmmo(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        if (slot != null)
+        {
+            slot.ammoAmount--;
+        }
+    }
+
+    public bool NeedsReload(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        if (slot == null) return false;
+
+        // Cần nạp đạn khi: đạn trong băng chưa đầy VÀ còn đạn dự trữ
+        return slot.ammoAmount < slot.maxAmmo && slot.reserveAmmo > 0;
+    }
+
+    public bool HasAmmoToReload(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        return slot != null && slot.reserveAmmo > 0;
+    }
+
+    public void ReloadAmmo(AmmoType ammoType)
+    {
+        AmmoSlot slot = GetAmmoSlot(ammoType);
+        if (slot != null && slot.reserveAmmo > 0)
+        {
+            // Tính số đạn cần để nạp đầy băng
+            int ammoNeeded = slot.maxAmmo - slot.ammoAmount;
+
+            // Kiểm tra xem có đủ đạn dự trữ không
+            int ammoToAdd = Mathf.Min(ammoNeeded, slot.reserveAmmo);
+
+            // Nạp đạn và trừ đạn dự trữ
+            slot.ammoAmount += ammoToAdd;
+            slot.reserveAmmo -= ammoToAdd;
+        }
+    }
+
+    private AmmoSlot GetAmmoSlot(AmmoType ammoType)
+    {
+        foreach (AmmoSlot slot in ammoSlots)
+        {
+            if (slot.type == ammoType) return slot;
+        }
+        return null;
+    }
+}
---
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSwitcher : MonoBehaviour
+{
+    [SerializeField] public int currentWeapon = 0;
+
+    public event Action<int> OnWeaponChanged; // Sự kiện thông báo vũ khí đã thay đổi
+
+    void Start()
+    {
+        SetWeaponActive();
+    }
+
+    private void SetWeaponActive()
+    {
+        int weaponIndex = 0;
+        foreach (Transform weapon in transform)
+        {
+            bool isActive = weaponIndex == currentWeapon;
+            weapon.gameObject.SetActive(isActive);
+
+            weaponIndex++;
+        }
+    }
+
+    void Update()
+    {
+        int previousWeapon = currentWeapon;
+
+        ProcessKeyInput();
+
+        if (previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+            OnWeaponChanged?.Invoke(currentWeapon); // Gọi sự kiện khi đổi vũ khí
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            currentWeapon = 3;
+        }
+    }
+}
